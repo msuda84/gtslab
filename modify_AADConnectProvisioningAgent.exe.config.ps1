63e7a1285bba677f12d25a3e2cd92749dc6d49d2
msuda84 <70759212+msuda84@users.noreply.github.com>
2022-08-23 11:25:01 +0200

    Delete modify_AADConnectProvisioningAgent.exe.config.ps1

--- modify_AADConnectProvisioningAgent.exe.config.ps1
+++
@@ -1,3 +0,0 @@
-$file = ((Get-Content 'C:\Program Files\Microsoft Azure AD Connect Provisioning Agent\AADConnectProvisioningAgent.exe.config') -as [Collections.ArrayList])
-$file.insert(3,'    <enforceFIPSPolicy enabled='`$false'/>')
-$file | Set-Content 'C:\Program Files\Microsoft Azure AD Connect Provisioning Agent\AADConnectProvisioningAgent.exe.config' -force
